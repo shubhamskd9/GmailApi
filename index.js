d3a782346baea410d14bea040b69fa13643488ea
Shubham Dwivedi <shubhamsrd9@gmail.com>
2023-02-24 11:30:00 +0530

    final commit

--- index.js
+++ index.js
@@ -1,159 +1,96 @@
+const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
+const { readFile } = require("fs");
+const { promisify } = require("util");
+const { createServer } = require("http");
+const { URL } = require("url");
+const open = require("open");
 const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-let myLabel;
+const TOKEN_PATH = "./token.json";
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
+async function main() {
+  // Load token from file
+  const token = JSON.parse(await readFileAsync(TOKEN_PATH));
+
+  // Create OAuth2 client
+  const oAuth2Client = new OAuth2(
+    "315443154313-gbmg2q6hj2vbn5gk87vn001bf0c447oq.apps.googleusercontent.com",
+    "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",
+   "http://localhost",
+  );
+
+oAuth2Client.setCredentials(token);
+
+
+  // Create Gmail API client
+  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
+  fetchEmails(gmail);
+}
+
+
+
+async function fetchEmails(gmail) {
+  // List messages that don't have any prior replies
+  const query = "is:inbox -label:replied";
+  const {
+    data: { messages },
+  } = await gmail.users.messages.list({ userId: "me", q: query });
+
+  console.log(messages.length);
+  
+  // Reply to each message
+  for(let i = 0; i<messages.length-95;i++){
+
+    const id = messages[i].id;
+    const {
+      data: { payload },
+    } = await gmail.users.messages.get({ userId: "me", id });
+
+    // Check if the message has any prior replies
+    const thread = payload.headers.find(({ name }) => name === "In-Reply-To");
+    if (thread) {
+      console.log(`Skipping message ${id} because it has a prior reply.`);
+      continue;
+    }
+
+
+
+    // Extract the sender's email address and the message body
+    const from = payload.headers.find(({ name }) => name === "From").value;
+    // const body = payload.parts[0].body.data;
+
+    // Compose reply message
+    const reply = `Hello ${from},\n\nThank you for your email. This is not an automated response.\n\n`;
+  
+    console.log(messages[i]);
+
+
+    sendReply(gmail, id, messages[i].threadId, reply, payload, from);
+    
   }
 }
 
-/**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-
-  return client;
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listLabels(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
-  const res = await gmail.users.labels.list({
+async function sendReply(gmail, id, threadId, reply,payload, from) {
+  const sentMessage = await gmail.users.messages.send({
     userId: 'me',
-  });
-  const labels = res.data.labels;
-  if (!labels || labels.length === 0) {
-    console.log('No labels found.');
-    return;
-  }
-  console.log('Labels:');
-  labels.forEach((label) => {
-    if(label.name === "IMPORTANT"){
-        myLabel = label;
+    requestBody: {
+      threadId: threadId,
+      raw: Buffer.from(
+        `To: ${from}\nSubject: Re: ${
+          payload.headers.find(({ name }) => name === "Subject").value
+        }\n\n${reply}`
+      ).toString("base64")
     }
-    console.log(label.id);
   });
-
-  fetchEmails();
-
-}
+  console.log(`Sent reply to message ${id}: ${sentMessage.data.id}`);
 
 
-
-async function fetchEmails(auth) {
-    const gmail = google.gmail({ version: 'v1', auth: auth });
-    const res = await gmail.users.messages.list({ userId: 'me' });
-    const messages = res.data.messages || [];
-    console.log(messages);
-
-    const noReplyMessages = [];
-
-    for (let i = 0; i < messages.length-90; i++) {
-      const message = messages[i];
-      const messageDetails = await gmail.users.messages.get({ userId: 'me', id: message.id });
-      console.log(messageDetails);
-      if (!messageDetails.data.threadId) {
-        noReplyMessages.push(messageDetails);
-        sendReply(message.id, messageDetails);
-        tagSentEmail(message.id, myLabel);
-      }
-    }
+  tagSentEmail(gmail, id, "IMPORTANT");
 
 }
 
-async function sendReply(messageId, messageDetails) {
-    const from = messageDetails.payload.headers.find(header => header.name === 'From').value;
-    const to = messageDetails.payload.headers.find(header => header.name === 'To').value;
-    const subject = messageDetails.payload.headers.find(header => header.name === 'Subject').value;
-    const body = 'Hello, this is an automated reply.';
-    const threadId = messageDetails.threadId;
-  
-    const reply = await gmail.users.messages.send({
-      userId: 'me',
-      requestBody: {
-        threadId: threadId,
-        raw: createMessage(from, to, subject, body)
-      }
-    });
-    console.log(`Sent reply to message ${messageId}: ${reply.data.id}`);
-
-
-  }
-  
-  function createMessage(from, to, subject, body) {
-    const message = [
-      `From: ${from}`,
-      `To: ${to}`,
-      `Subject: ${subject}`,
-      `Thread-Id: ${threadId}`,
-      '',
-      `${body}`
-    ].join('\n');
-  
-    return Buffer.from(message)
-      .toString('base64')
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-}
-
-async function tagSentEmail(messageId, label) {
+async function tagSentEmail(gmail, messageId, label) {
     const res = await gmail.users.messages.modify({
       userId: 'me',
       id: messageId,
@@ -164,4 +101,44 @@ async function tagSentEmail(messageId, label) {
     console.log(`Tagged email with label ${label}: ${messageId}`);
 }
 
-authorize().then(listLabels).catch(console.error);
+async function getLabel(gmail, newLabel) {
+  try {
+    const res = await gmail.users.labels.list({
+      userId: "me",
+    });
+
+    const labels = res.data.labels;
+
+    // Check if label already exists
+    for (let i = 0; i < labels.length; i++) {
+      if (labels[i].name === newLabel) {
+        console.log("Label already exists");
+        return labels[i];
+      }
+    }
+
+    // If label doesn't exist, create it
+    const response = await gmail.users.labels.create({
+      userId: "me",
+      requestBody: {
+        name: newLabel,
+        labelListVisibility: "labelShow",
+        messageListVisibility: "show",
+      },
+    });
+
+    console.log("Label created:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Authorize and run main function
+(async () => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+  }
+})();
